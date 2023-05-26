3f274fa0da87d2f4797284612b367be46d0592d2
Renuka Fernando <renukapiyumal@gmail.com>
2023-05-26 17:14:09 +0530

    Set Set-Response-Status-Code and Set-Response-Time-Ms from headers

--- build-docker.sh
+++ build-docker.sh
@@ -4,4 +4,4 @@
 
 env GOOS=linux GOARCH=amd64 go build -o request-info-amd64 .
 env GOOS=linux GOARCH=arm64 go build -o request-info-arm64 .
-docker buildx build --push --platform linux/arm64/v8,linux/amd64 -t cakebakery/request-info:v1 .
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 -t cakebakery/request-info:v2 .
