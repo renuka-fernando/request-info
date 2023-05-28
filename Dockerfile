60b0a908b1e4e993092ccf9eb42937c16af7c476
Renuka Fernando <renukapiyumal@gmail.com>
2023-05-28 08:07:11 +0530

    Add CORS support and pass delayMs and status code in query params

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:3.12.1
+FROM alpine:3.18.0
 ARG TARGETARCH
 
 COPY request-info-$TARGETARCH /bin/request-info
--- build-docker.sh
+++ build-docker.sh
@@ -7,6 +7,5 @@ IMAGE_NAME=cakebakery/request-info:v2
 env GOOS=linux GOARCH=amd64 go build -o request-info-amd64 .
 env GOOS=linux GOARCH=arm64 go build -o request-info-arm64 .
 
-docker buildx build --push --platform linux/arm64/v8,linux/amd64 -t "$IMAGE_NAME" .
-#docker build --build-arg TARGETARCH=arm64 -t "$IMAGE_NAME" .
-
+#docker buildx build --push --platform linux/arm64/v8,linux/amd64 -t "$IMAGE_NAME" .
+docker build --build-arg TARGETARCH=arm64 -t "$IMAGE_NAME" .
