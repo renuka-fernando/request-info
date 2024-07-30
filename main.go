627334024e958bbbac2eb8607b636cf07c32ea51
Renuka Fernando <renukapiyumal@gmail.com>
2024-07-30 09:01:16 +0530

    refactor: Update HTTP status code flag name to be more descriptive

--- main.go
+++ main.go
@@ -265,7 +265,7 @@ func main() {
 	flag.StringVar(&key, "key", "server.key", "Key file for HTTPS server")
 	flag.StringVar(&clientCA, "ca", "ca.crt", "CA certificate file for client verification")
 	flag.IntVar(&delayMs, "delayMs", 0, "Time to wait (ms) before responding to request")
-	flag.IntVar(&statusCode, "status", 200, "HTTP status code to respond")
+	flag.IntVar(&statusCode, "statusCode", 200, "HTTP status code to respond")
 	flag.BoolVar(&setResponseBody, "setResponseBody", false, "Response the body set via -responseBody flag")
 	flag.StringVar(&responseBody, "responseBody", "", "The response body to return")
 	flag.BoolVar(&disableAccessLogs, "disable-access-logs", false, "Disable access logs")
